d3c15a8b956efb77cd0fecedb479c600c7611a60
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-01 18:12:28 +0530

    completes basic skleton with rqrd dependencies

--- src/components/QuoteGenerator.js
+++ src/components/QuoteGenerator.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const QuoteGenerator = () => {
   return (
-   <div id="quote-box"></div>
+   <div id="quote-box">
+    <div id="text"></div>
+    <div id="author"></div>
+    <button id="new-quote"></button>
+    <a href="twitter.com/intent/tweet" id="tweet-quote"></a>
+   </div>
   )
 }
